6b30f5f4106327c10810fa39b53ad787f97447fe
DavidASC20 <djchong24@gmail.com>
2022-01-10 20:42:28 -0500

    finished

--- control.c
+++ control.c
@@ -25,14 +25,14 @@ void rem();
 int main(int argc, char *argv[]) {
 
   if (argv[1] == NULL) {
-    printf("Add -c to CREATE a file, or -r to REMOVE a file\n");
+    printf("use -create and -remove\n");
     return 0;
   }
 
-  if (strcmp(argv[1], "-c") == 0) {
+  if (strcmp(argv[1], "-create") == 0) {
     create();
   }
-  else if (strcmp(argv[1], "-r") == 0) {
+  else if (strcmp(argv[1], "-remove") == 0) {
     rem();
   }
 
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 }
 
 void create() {
-  printf("Creating File\n");
+  printf("creating file\n");
 
   int semd = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
   if (semd == -1) {
@@ -50,25 +50,25 @@ void create() {
   union semun us;
   us.val = 1;
   int x = semctl(semd, 0, SETVAL, us);
-  printf("semctl Returned %d\n", x);
+  printf("returned semctl %d\n", x);
 
   int shmd = shmget(SHM_KEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
   if (shmd == -1) {
     shmd = shmget(SHM_KEY, 0, 0);
   }
 
-  int file = open("tmp.txt", O_CREAT | O_EXCL | O_TRUNC, 0644);
+  int file = open("a.txt", O_CREAT | O_EXCL | O_TRUNC, 0644);
   if (file == -1) {
-    file = open("tmp.txt", O_RDWR);
+    file = open("a.txt", O_RDWR);
   }
 
   close(file);
 
-  printf("Create Success\n");
+  printf("successful creation\n");
 }
 
 void rem() {
-  printf("Removing File\n");
+  printf("removing file\n");
 
   int semd = semget(SEM_KEY, 0, 0);
   if (semd == -1) {
@@ -86,7 +86,7 @@ void rem() {
 
   int file = open("tmp.txt", O_RDONLY);
   if (file == -1) {
-    printf("Failed Opening\n");
+    printf("Oops, didnt open \n");
     return;
   }
 
@@ -96,6 +96,6 @@ void rem() {
 
   read(file, text, sb.st_size);
 
-  printf("File Contents:\n");
+  printf("file:\n");
   printf("%s\n", text);
 }
